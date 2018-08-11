414a873c4a13a4828134f38cafdaa453841a17e4
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2018-08-11 03:09:15 -0400

    store_message.h, main.c files.h: store_message stores message in generated files between sender and receiver, main file modified to reflect changes, files.h deleted because redundant.

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 #include <string.h>
 #include "directories.h"
 #include "userlist.h"
-#include "files.h"
 #include "username_check.h"
 #include "userauth.h"
 #include "usercred.h"
+#include "store_message.h"
 
 
 int main()
 {
+    int flag = 0;
     printf("1. Sign up.\n2. Log in.\n");
     int op;
     scanf("%d", &op);
@@ -32,12 +33,10 @@ int main()
             directories(username);
             userlist(username);
             usercred(username, password);
-            files();
         }
     }
     else if ( op == 2 ) //LOGIN
     {
-        int flag = 0;
         char username[100];
         char password[100];
         printf("Enter username\n");
@@ -62,6 +61,40 @@ int main()
                 flag = 0;
                 printf("Password mismatch. Try again.\n");
                 return;
+            }
+        }
+        if ( flag == 1 )
+        {
+            printf("1. Send message.\n2. View message.\n");
+            int subop;
+            scanf("%d", &subop);
+            if ( subop == 2 )
+            {
+                printf("OOPS. PLACEHOLDER.\n");
+                return;
+            }
+            else if ( subop ==1 )
+            {
+                char sender[100];
+                strcpy(sender, username);
+                printf("Enter name of receiver.\n");
+                char receiver[100];
+                scanf(" %s", receiver);
+                if ( ( username_check(receiver) ) == 1 )
+                {
+                    printf("Receiver found.\n");
+                }
+                else
+                {
+                    printf("Receiver not found in list of users. Try again.\n");
+                    return;
+                }
+                printf("Enter message.\n");
+                char text[10000];
+                getchar();
+                gets(text);
+                store_in_sender(sender, receiver, text);
+                printf("Done.\n");
             }
         }
     }
